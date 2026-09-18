9d951c72302d28e2d5806cd04fcaed6721d247b7
Lakshya <lakshyasharmaktl@gmail.com>
2026-09-18 18:07:32 +0530

    sdfg

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -4,9 +4,9 @@ import { ToastProvider } from "./context/ToastContext";
 import { AuthProvider } from "./context/AuthContext";
 import { CartProvider } from "./context/CartContext";
 
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import CartDrawer from "./components/CartDrawer";
+import Navbar from "./components/Navbar.jsx";
+import Footer from "./components/Footer.jsx";
+import CartDrawer from "./components/CartDrawer.jsx";
 
 import HomePage from "./pages/HomePage";
 import ShopPage from "./pages/ShopPage";
